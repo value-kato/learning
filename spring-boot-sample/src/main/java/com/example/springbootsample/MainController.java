18611730fe1f9ec2133751252b409f77adbdfdc7
value-kato <>
2023-04-04 16:02:02 +0900

    2に変更

--- spring-boot-sample/src/main/java/com/example/springbootsample/MainController.java
+++ spring-boot-sample/src/main/java/com/example/springbootsample/MainController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
-    @GetMapping("/test1")
+    @GetMapping("/test2")
     public String write1(Model model) {
-        model.addAttribute("message", "hello world!");
+        model.addAttribute("message", "hello world2!");
         return "hello";
     }
 }
