4c63b1335ceea4184a08410d8f4db5912700bc66
value-kato <>
2023-04-05 13:41:24 +0900

    値はvalueですを表示

--- spring-boot-sample/src/main/java/com/example/springbootsample/ParamController.java
+++ spring-boot-sample/src/main/java/com/example/springbootsample/ParamController.java
@@ -1,13 +1,17 @@
 package com.example.springbootsample;
 
+import ch.qos.logback.core.model.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ParamController {
+
     @GetMapping("/hoge")
-    public String hoge(@RequestParam("param") String value){
-        return value;
+    public String hoge(@RequestParam(value = "param", required = true) String value) {
+        return "値は" + value + "です";
     }
+
+
 }
